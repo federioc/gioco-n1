49197f1773c4bb250d83f8e5888cba8ba16c5b62
federioc <74595780+federioc@users.noreply.github.com>
2021-01-17 23:43:00 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    otherSprite.setPosition(randint(0, 160), randint(0, 120))
+})
+scene.setBackgroundColor(1)
+let personaggio = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 6 6 6 6 6 6 6 6 . . 
+    . . . . . 6 c 6 6 6 6 6 6 9 6 . 
+    . . . . 6 c c 6 6 6 6 6 6 9 c 6 
+    . . d 6 9 c c 6 9 9 9 9 9 9 c c 
+    . d 6 6 9 c b 8 8 8 8 8 8 8 6 c 
+    . 6 6 6 9 b 8 8 b b b 8 b b 8 6 
+    . 6 6 6 6 6 8 b b b b 8 b b b 8 
+    . 6 6 6 6 8 6 6 6 6 6 8 6 6 6 8 
+    . 6 d d 6 8 f 8 8 8 f 8 8 8 8 8 
+    . d d 6 8 8 8 f 8 8 f 8 8 8 8 8 
+    . 8 8 8 8 8 8 8 f f f 8 8 8 8 8 
+    . 8 8 8 8 f f f 8 8 8 8 f f f f 
+    . . . 8 f f f f f 8 8 f f f f f 
+    . . . . f f f f . . . . f f f . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(personaggio)
+let pizza = sprites.create(img`
+    . . . . . . b b b b . . . . . . 
+    . . . . . . b 4 4 4 b . . . . . 
+    . . . . . . b b 4 4 4 b . . . . 
+    . . . . . b 4 b b b 4 4 b . . . 
+    . . . . b d 5 5 5 4 b 4 4 b . . 
+    . . . . b 3 2 3 5 5 4 e 4 4 b . 
+    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+    . . . b 5 3 2 3 5 5 5 5 e e e e 
+    . . b d 7 5 5 5 3 2 3 5 5 e e e 
+    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+    b d 3 2 d 5 5 5 d d d 4 4 . . . 
+    b 5 5 5 5 d d 4 4 4 4 . . . . . 
+    4 d d d 4 4 4 . . . . . . . . . 
+    4 4 4 4 . . . . . . . . . . . . 
+    `, SpriteKind.Food)
+pizza.setPosition(17, 17)
+info.startCountdown(30)
